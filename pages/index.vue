f322528068da3d6cb3316f0144d34dd9db0ee8f6
O H <olle@unremarkablegarden.com>
2021-10-22 19:26:53 +0200

    loader

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 #index(v-if='doc')
   logo(:hide='hide')
   .project(v-for='(p, i) in doc.project', v-if='p.link.url')
+    .loader
     nuxt-link(:to='p.link.url')
       player(v-if='p.vimeo_url', :data='p', type='loop')
 </template>
@@ -43,3 +44,19 @@ export default {
   }
 }
 </script>
+
+<style lang="sass" scoped>
+.project
+  height: calc(100vw / 16*9) // video size
+  background: black
+  .loader
+    position: absolute
+    width: 100vw
+    height: calc(100vw / 16*9)
+    background: white
+    background-image: url(/loader.gif)
+    background-size: 20px auto
+    background-position: center center
+    background-repeat: no-repeat
+    filter: invert(100)
+</style>
