f689d9cc7b9599fb6b3dd1ad943e0b29c6621be5
O H <olle@unremarkablegarden.com>
2021-12-11 15:26:13 +0100

    player fix

--- plugins/vue-plyr.js
+++ plugins/vue-plyr.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import VuePlyr from '@/modules_patched/vue-plyr/dist/vue-plyr.ssr.js'
+import VuePlyr from './vue-plyr/dist/vue-plyr.ssr.js'
 // import 'vue-plyr/dist/vue-plyr.css'
 
 // The second argument is optional and sets the default config values for every player.
