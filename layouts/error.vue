7f3340590f66ba77a955a37dead7cf30f5657c87
unremarkablegarden <olle@unremarkablegarden.com>
2023-06-09 16:47:43 +0200

    error

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,7 +3,8 @@
   .inner.w-full.h-full.flex.justify-center.items-center
     .text-center
       p.text-gray-600
-        | The page couldn't be found
+        p(v-if='error.statusCode === 404') Page not found
+        p(v-else) An error occurred
       .text-gray-600.flex.justify-center.items-center
         div
           .text-xl.-mb-1.mr-1 &larr; 
@@ -11,3 +12,20 @@
           nuxt-link(to="/")
             | Index
 </template>
+
+<script>
+export default {
+  props: ['error'],
+  head: {
+    title: 'Page not found'
+  },
+  mounted () {
+    console.log(this.error);
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.h-full
+  height: 90vh
+</style>
